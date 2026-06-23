7b6fe321c6768a6088fcac233d87a9694feb40b5
DesenvolvimentoFFA1 <desenvolvimento@ffainfraestrutura.com.br>
2026-06-23 10:56:37 -0300

    Fix: Correção do envio de arquivo na solicitação de compra

--- backend/app/Http/Controllers/UploadsController.php
+++ backend/app/Http/Controllers/UploadsController.php
@@ -954,7 +954,7 @@ class UploadsController extends Controller
 
             // Caminho completo do arquivo
             $caminhoCompleto = "documentos_compras/{$codCompra}/{$nomeArquivo}";
-            $tamanhoArquivo = $file->getSize();
+            // $tamanhoArquivo = $file->getSize();
 
             // Mover arquivo para a pasta
             $file->move($pasta, $nomeArquivo);
@@ -971,7 +971,7 @@ class UploadsController extends Controller
                 'nome_arquivo' => $nomeArquivo,
                 'nome_hash' => $nomeHash,
                 'caminho' => $caminhoCompleto,
-                'tamanho' => $tamanhoArquivo,
+                'tamanho' => 0,
                 'extensao' => $extensao,
                 'ordem' => $ordem,
                 'matricula_upload' => $matricula,
