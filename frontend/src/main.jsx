e4c0aaab3e9eedefedeb0662a622326c116e867e
DesenvolvimentoFFA1 <suportedesenvolvimento@ffainfraestrutura.com>
2026-06-23 09:39:56 -0300

    Merge: Resolução de conflitos entre development e production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,11 +8,7 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-<<<<<<< HEAD
-      <BrowserRouter basename="/compras">
-=======
       <BrowserRouter basename="/">
->>>>>>> a407070151f2236f6c2396cb7b70326c51b418c0
         <App />
       </BrowserRouter>
     </React.StrictMode>
