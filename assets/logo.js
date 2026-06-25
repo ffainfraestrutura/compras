fc2e8ab116184e5caa8b9476e107658dc4fe70d2
DesenvolvimentoFFA1 <desenvolvimento@ffainfraestrutura.com.br>
2026-06-25 14:49:04 -0300

    Fix: Correção do caminho da pasta assets

---
+++ assets/logo.js
@@ -0,0 +1,1 @@
+const o="/assets/logo.png";export{o as L};
---
+++ assets/useFiliais.js
@@ -0,0 +1,1 @@
+import{r as a,a as n}from"./index.js";function f(){const[i,r]=a.useState([]),[o,s]=a.useState(!1),[c,l]=a.useState(!1),t="https://compras.painel-telecom.com/backend/public/api/filiais";return a.useEffect(()=>{s(!0),n.get(t).then(e=>r(e.data)).catch(e=>{console.error("Erro ao buscar filiais:",e),l(!0)}).finally(()=>s(!1))},[t]),{filiais:i,loading:o,error:c}}export{f as u};
